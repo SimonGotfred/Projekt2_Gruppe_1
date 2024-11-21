d7a0212ab25102bbca0decf1cc0854f13e2f2563
albo0003 <albo0003@stud.kea.dk>
2024-11-21 14:23:41 +0100

    MemberRegister anden itteration, ændring af telefonnummer osv.

--- src/MemberRegister.java
+++ src/MemberRegister.java
@@ -1,38 +1,46 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class MemberRegister {
-    static ArrayList<Medlem> members = new ArrayList<>();
-    static Scanner sc = new Scanner(System.in);
+    static ArrayList<Member> members = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+    static String format = "dd MM yyyy";
+    static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
 
     public static void main(String[] args) {
         addMemberMenu();
     }
     static void addMemberMenu(){
         String name;
-        int age;
+        String tlfNr;
+        LocalDate birthday;
         boolean isActive;
         String disciplin = "";
         boolean isCompeting = false;
         System.out.println("Skriv Medlemmets Navn: ");
-        name = sc.nextLine();
-        System.out.println("Skriv Medlemmets Alder: ");
-        age = checkIfInt();
-        System.out.println("Er Medlemmet Aktivt?: ");
+        name = scanner.nextLine();
+        System.out.println("Skriv Medlemmets Tlf Nr.: ");
+        tlfNr = checkTlfNr();
+        System.out.println("Skriv Medlemmets fødselsdag: ");
+        birthday = checkIfDate();
+        System.out.println("Er Medlemmet Aktivt? (ja/nej): ");
         isActive = checkYesOrNo();
         if(isActive){
             System.out.println("Hvad Er Medlemmets Disciplin?: ");
-            disciplin = sc.nextLine();
-            System.out.println("Er Medlemmet En Konkurrencesvømmer?: ");
+            disciplin = scanner.nextLine();
+            System.out.println("Er Medlemmet En Konkurrencesvømmer? (ja/nej): ");
             isCompeting = checkYesOrNo();
         }
-        System.out.println(name + ", " + age + ", er aktiv: " + isActive + ", " + disciplin + ", er konkurrenceSvømmer: " + isCompeting);
-        members.add(new Medlem());
+        System.out.println(name + ", " + birthday + ", er aktiv: " + isActive + ", " + disciplin + ", er konkurrenceSvømmer: " + isCompeting);
+        members.add(new Member(name, birthday, tlfNr));
     }
     static boolean checkYesOrNo(){
         while (true) {
-            String activity = sc.nextLine();
+            String activity = scanner.nextLine().toLowerCase();
             if (activity.equals("ja"))
                 return true;
             else if (activity.equals("nej"))
@@ -40,16 +48,40 @@ public class MemberRegister {
             System.out.println("forkert input, prøv igen");
         }
     }
-    static int checkIfInt(){
+    static LocalDate checkIfDate(){
         while (true){
             try {
-                int i = sc.nextInt();
-                sc.nextLine();
-                return i;
+                String birthD = scanner.nextLine();
+
+                return LocalDate.parse(birthD, dateTimeFormatter);
+            }
+            catch (DateTimeParseException e){
+                System.out.println("forkert input, prøv igen: ");
+            }
+        }
+    }
+    static String checkTlfNr(){
+        while (true){
+            try {
+                int number = scanner.nextInt();
+                String numberString = "" + number;
+                scanner.nextLine();
+                boolean nAlreadyExist = false;
+                for (int i = 0; i < members.size(); i++){
+                    if (numberString.equals(members.get(i).phoneNumber))
+                        nAlreadyExist = true;
+                }
+                if (("" + number).length() == 8 && !nAlreadyExist){
+                    return numberString;
+                }
+                if (nAlreadyExist)
+                    System.out.println("der er allerede et medlem under dette telefon nummer");
+                else
+                    System.out.println("ikke et rigtigt telefon nummer, prøv igen: ");
             }
             catch (InputMismatchException e){
-                System.out.println("forkert input, prøv igen int");
-                sc.nextLine();
+                System.out.println("ikke et rigtigt telefon nummer, prøv igen: ");
+                scanner.nextLine();
             }
         }
     }
