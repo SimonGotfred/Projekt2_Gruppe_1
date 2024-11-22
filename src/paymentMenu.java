b33de00d19a39b6a11d379254e3b55b7d0ace00a
sabirinisa <sais0001@stud.kea.dk>
2024-11-22 12:59:03 +0100

    har oprettet en payment class

--- src/paymentMenu.java
+++ src/paymentMenu.java
@@ -1,4 +1,28 @@
-public class paymentMenu {
+import java.time.LocalDate;
 
+public class PaymentMenu {
+    public static void main(String[] args) {
+        MemberRegister.members.add(new Member("John Doe", LocalDate.of(2010,10,10),"12121212"));
+        MemberRegister.members.add(new Member("Ida Doe", LocalDate.of(2010,10,10),"12121212"));
+        MemberRegister.members.add(new Member("Carl Doe", LocalDate.of(2010,10,10),"12121212"));
 
+        MemberRegister.members.getFirst().pay(600);
+
+        paymentMenu();
+    }
+    public static void paymentMenu(){
+        System.out.println("Velkommen til Payment menuen");
+        for (Member member : MemberRegister.members){
+            System.out.println(member.name + " " + paymentStatus(member.hasPaid()));
+        }
+    }
+
+    public static String paymentStatus(boolean hasPaid){
+        if (hasPaid){
+            return "Har betalt";
+        }
+        else{
+            return "Har ikke betalt";
+        }
+    }
 }
