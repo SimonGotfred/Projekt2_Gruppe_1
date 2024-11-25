641223c056db29f4d849c56d264b85dfab784c52
sabirinisa <sais0001@stud.kea.dk>
2024-11-25 13:31:37 +0100

    Tilføjet mulighed for at betale

--- src/PayMenu.java
+++ src/PayMenu.java
@@ -1,7 +1,8 @@
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class PayMenu {
-    public static void main(String[] args) {
+    public static void main(String[] args) { // testing
         MemberRegister.members.add(new Member("John Doe", LocalDate.of(2010,10,10),"12121212"));
         MemberRegister.members.add(new Member("Ida Doe", LocalDate.of(2010,10,10),"12121212"));
         MemberRegister.members.add(new Member("Carl Doe", LocalDate.of(2010,10,10),"12121212"));
@@ -9,20 +10,51 @@ public class PayMenu {
         MemberRegister.members.getFirst().pay(600);
 
         paymentMenu();
+
+
     }
+
     public static void paymentMenu(){
         System.out.println("Velkommen til Payment menuen");
         for (Member member : MemberRegister.members){
-            System.out.println(member.name + " " + paymentStatus(member.hasPaid()));
+            System.out.println(member.name + " " + paymentStatus(member));
+        }
+        boolean run = true;
+        while (run){
+           run = makePayment();
         }
     }
 
-    public static String paymentStatus(boolean hasPaid){
-        if (hasPaid){
+    public static String paymentStatus(Member member){
+        if (member.hasPaid()){
             return "Har betalt";
         }
         else{
-            return "Har ikke betalt";
+            return "Har ikke betalt. " + "Personen skylder " + member.paymentOwed() + " Kr";
         }
     }
+
+    public static boolean makePayment(){
+        System.out.println("\nSkriv navnet på personen, der skal betale regningen");
+        Member member = null;
+        while (member == null ){
+            String name = MemberRegister.scanner.nextLine().toLowerCase();
+
+            for (Member m : MemberRegister.members){
+                if (m.name.toLowerCase().equals(name)){
+                    member=m;
+                    break;
+                }
+            }
+
+            if (member != null){
+                member.pay(member.paymentOwed());
+                System.out.println("Nu har du betalt");
+                return true;
+            }
+            System.out.println("Du har tastet forkert, prøv igen");
+        }
+        return true;
+    }
 }
+// sæt en while lykke der omfatter blokken
