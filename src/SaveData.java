7f50b5a355b62a29f3c0dbf873f4dd5f9aefd615
albo0003 <albo0003@stud.kea.dk>
2024-11-26 12:13:45 +0100

    save data

---
+++ src/SaveData.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class SaveData {
+    static String format = "yyyy-MM-dd";
+    static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern(format);
+    public static void main(String[] args) throws IOException {
+       /* MemberRegister.members.add(new Member("alex", LocalDate.now().minusYears(18),"00110011"));
+        MemberRegister.members.add(new Member("pedro", LocalDate.now().minusYears(18),"98899889"));
+        MemberRegister.members.add(new Member("sarbjit", LocalDate.now().minusYears(18),"10011001"));
+        saveData();*/
+        makeMembersFromData();
+        for (Member m : MemberRegister.members){
+            System.out.println(m.getName() + ",  " + m.getPhoneNumber());
+        }
+    }
+    static void makeMembersFromData() throws IOException {
+        FileReader file = new FileReader("src/data.txt");
+        BufferedReader reader = new BufferedReader(file);
+        String line = reader.readLine();
+        while (line != null){
+            String[] var = line.split(",");
+            MemberRegister.members.add(new Member(var[0], LocalDate.parse(var[1], dateTimeFormatter), var[2]));
+
+            line = reader.readLine();
+        }
+
+    }
+    static void saveData() throws IOException{
+        PrintWriter ud = new PrintWriter(new FileWriter("src//data.txt"));
+        for (Member m : MemberRegister.members){
+            ud.println(m.getName() + ","+ m.getBirthDate()+","+m.getPhoneNumber());
+        }
+        ud.close();
+    }
+}
