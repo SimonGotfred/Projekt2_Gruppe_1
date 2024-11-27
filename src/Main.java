0c0096c8cc14324cc9716bf9668603e0be295dd4
Simon Gotfred <simongotfred@hotmail.com>
2024-11-27 15:41:06 +0100

    Simplified 'Main'

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("jeg er alex");
-
-        Menu menu = new Menu();
-        menu.start();
+        Menu.start();
     }
 }
