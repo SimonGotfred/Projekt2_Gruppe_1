3a7638e4655c098e986b0e1708271cf33f025cfb
sabirinisa <sais0001@stud.kea.dk>
2024-12-03 12:21:00 +0100

    har lavet edit motoden

--- src/EditMember.java
+++ src/EditMember.java
@@ -1,9 +1,24 @@
 public class EditMember {
-    public class EditMember{
+
+    public static void edit(){
+        Member member = Sorter.chooseMember(MemberRegister.members);
+        System.out.println(member);
+        String input = UI.inquire("Vil du redigere telefonnummeret eller om de er aktiv/passiv");
+        switch (input.toLowerCase()){
+            case "telefon nummer", "tlf" : editTelefonnummer(member); break;
+            case "aktiv": member.setActive(); break;
+            case "passiv": member.isPassive(); break;
+        }
 
     }
-    public static void edit(){
-        System.out.println("Do u wanna edit a member?");
-
+    public static void editTelefonnummer(Member member){
+        String input = UI.inquire("Hvad er det nye telefon nummer du gerne vil redigere?");
+        if (Member.isPhoneNumber(input)){
+            member.setPhoneNumber(input);
+        }
+        else {
+            System.out.println("Forkert formateret, prøv igen");
+            editTelefonnummer(member);
+        }
     }
 }
