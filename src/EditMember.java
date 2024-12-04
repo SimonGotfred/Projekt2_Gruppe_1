7853aa658d6558cc0e46e2e2440b8755121e2538
sabirinisa <sais0001@stud.kea.dk>
2024-12-04 11:25:16 +0100

    har ændret

--- src/EditMember.java
+++ src/EditMember.java
@@ -4,12 +4,13 @@ public class EditMember {
         Member member = Sorter.chooseMember(MemberRegister.members);                                 // Vælg et medlem fra medlemsregisteret vha. Sorter.chooseMember
         System.out.println(member);                                                                  // Udskriv det valgte medlems informationer
        while (true){
+           System.out.println("Her kan du ændre på telefonnummer, om medlem er aktiv eller passiv eller slette");
            String input = UI.inquire("Vil du redigere telefonnummeret eller om de er aktiv/passiv");    // Spørg brugeren, hvad de vil redigere: telefonnummer eller status
            switch (input.toLowerCase()){                                                                // Brug en switch-case til at håndtere brugerens valg
                case "telefon nummer", "tlf" : editTelefonnummer(member); break;                         // Kald metoden til at redigere telefonnummeret
                case "aktiv": member.setActive(); break;
                case "passiv": member.isPassive(); break;
-               case "slet" : MemberRegister.members.remove(member); break;
+               case "slet", "delete" : MemberRegister.members.remove(member); break;
                default:
                    System.out.println("Ugyldigt valg, prøv igen.");                                     // Håndterer ugyldige indtastninger
            }
