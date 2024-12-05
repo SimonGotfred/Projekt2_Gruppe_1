8a87951a1bf4ed53dc8f806d36e5874d91dfb2ca
albo0003 <albo0003@stud.kea.dk>
2024-12-05 11:23:57 +0100

    Memberlist Private

--- src/DeleteMember.java
+++ src/DeleteMember.java
@@ -5,22 +5,27 @@ public class DeleteMember {
 
     static void deleteMemberMenu(){
         System.out.println("Du kan altid skrive 'q' for at gå tilbage til menuen");
-        System.out.println("Hvad er telefonNummeret af personen du vil afmelde?");
         checkTlfNr();
+        //delete member and save data
     }
     static void checkTlfNr(){
-        while (true){
-                String numberString = MemberRegister.scanner.nextLine();
-                if (numberString.toLowerCase(Locale.ROOT).equals("q"))
-                    return;
+        try {
+            while (true){
+                String numberString = UI.inquire("Hvad er telefonNummeret af personen du vil afmelde?");
                 for (int i = 0; i < MemberRegister.members.size(); i++){
                     if (numberString.equals(MemberRegister.members.get(i).getPhoneNumber())){
                         System.out.println("Medlemmet: "+ MemberRegister.members.get(i).getName() + "s abonnement er blevet annulleret");
                         MemberRegister.members.remove(MemberRegister.members.get(i));
+                        SaveData.saveData();
                         return;
                     }
                 }
                 System.out.println("Telefon nummeret høre ikke til noget medlem, prøv igen:");
+            }
         }
+        catch (ExitMenuCommand e){
+
+        }
+
     }
 }
