98ba1ea5820cbb6b3ae914045a6d5475a5373474
sabirinisa <sais0001@stud.kea.dk>
2024-12-05 11:26:32 +0100

    ny metode

--- src/EditMember.java
+++ src/EditMember.java
@@ -1,22 +1,26 @@
 public class EditMember {
 
-    public static void edit(){
+    public static void edit() throws ExitMenuCommand{
         Member member = Sorter.chooseMember(MemberRegister.members);                                 // Vælg et medlem fra medlemsregisteret vha. Sorter.chooseMember
         System.out.println(member);                                                                  // Udskriv det valgte medlems informationer
        while (true){
-           System.out.println("du kan taste tlf for at ændre telefon nummer og delete for slette");
+           System.out.println("Du kan taste tlf for at ændre telefon nummer, aktiv for aktivstatus, passiv for passivstatus og delete for slette og k for konkurrence");
            String input = UI.inquire("Vil du redigere telefonnummeret eller om de er aktiv/passiv");    // Spørg brugeren, hvad de vil redigere: telefonnummer eller status
-           switch (input.toLowerCase()){                                                                // Brug en switch-case til at håndtere brugerens valg
-               case "telefon nummer", "tlf" : editTelefonnummer(member); break;                         // Kald metoden til at redigere telefonnummeret
-               case "aktiv": member.setActive(); break;
-               case "passiv": member.isPassive(); break;
-               case "slet", "delete" : MemberRegister.members.remove(member); break;
-               default:
+           try {
+
+                 switch (input.toLowerCase()){                                                                // Brug en switch-case til at håndtere brugerens valg
+                 case "telefon nummer", "tlf" : editTelefonnummer(member); break;                         // Kald metoden til at redigere telefonnummeret
+                 case "aktiv": member.setActive(); break;
+                 case "passiv": member.setPassive(); break;
+                 case "slet", "delete" : MemberRegister.members.remove(member); break;
+                 case "konkurrence", "k" : member.setCompetitor(); break;
+                 default:
                    System.out.println("Ugyldigt valg, prøv igen.");                                     // Håndterer ugyldige indtastninger
-           }
+                }
+           } catch (ExitMenuCommand e) {}
        }
     }
-    public static void editTelefonnummer(Member member){                                            // Metode til at redigere et medlems telefonnummer
+    public static void editTelefonnummer(Member member) throws ExitMenuCommand {                                            // Metode til at redigere et medlems telefonnummer
         String input = UI.inquire("Hvad er det nye telefon nummer du gerne vil redigere?");         // Spørg brugeren om det nye telefonnummer
         if (Member.isPhoneNumber(input)){                                                           // Tjek, om telefonnummeret er korrekt formateret
             member.setPhoneNumber(input);                                                           // Opdater medlemmets telefonnummer
@@ -24,6 +28,16 @@ public class EditMember {
         else {
             System.out.println("Forkert formateret, prøv igen");                                    // Hvis formateringen er forkert, informer brugeren og prøv igen
             editTelefonnummer(member);
+        }
+    }
+    public static void editname(Member member) throws ExitMenuCommand {
+        String input = UI.inquire("Hvad er navnet du vil ændre til?");
+        if (Member.isName(input)){
+            member.setName(input);
+        }
+        else{
+            System.out.println("Forkert formateret, prøv igen");
+            editname(member);
         }
     }
 }
