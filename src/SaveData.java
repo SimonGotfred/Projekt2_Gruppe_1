89081b9c413a36ddc77b7515dcf41852ad35de71
Simon Gotfred <simongotfred@hotmail.com>
2024-12-06 11:58:18 +0100

    Minor fix in 'SaveData'
    - fixed use of deprecated constructor

--- src/SaveData.java
+++ src/SaveData.java
@@ -22,18 +22,18 @@ public class SaveData {
             BufferedReader reader = new BufferedReader(file);
             String line = reader.readLine();
             while (line != null){
-                String[] Performances = line.split("/");
-                System.out.println(Performances.length);
-                String[] var = Performances[0].split(",");
+                String[] performances = line.split("/");
+                System.out.println(performances.length);
+                String[] var = performances[0].split(",");
                 boolean isActive = false;
                 if (var[5].equals("True"))
                     isActive = true;
-                Member member = new Member(var[0], LocalDate.parse(var[1], dateTimeFormatter), var[2], isActive);
+                Member member = Member.newCompetitor(var[0], LocalDate.parse(var[1], dateTimeFormatter), var[2]);
                 member.charge(Double.parseDouble(var[3]));
 
-                if (Performances.length > 1){
-                    for (int i = 1; i < Performances.length; i++){
-                        String[] pVar = Performances[i].split(",");
+                if (performances.length > 1){
+                    for (int i = 1; i < performances.length; i++){
+                        String[] pVar = performances[i].split(",");
                         member.addPerformance(new Performance(Discipline.valueOf(pVar[0].toUpperCase()), LocalDateTime.parse(pVar[4], dateTimeFormatter), pVar[1], Integer.parseInt(pVar[3]), Integer.parseInt(pVar[5]),pVar[2]));
                     }
                 }
