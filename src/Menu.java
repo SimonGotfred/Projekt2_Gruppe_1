1e9db7023416622015ba5526b0dab689b6030ad1
Simon Gotfred <simongotfred@hotmail.com>
2024-12-10 12:00:03 +0100

    Added ExitMenuCommand to Menu

--- src/Menu.java
+++ src/Menu.java
@@ -8,119 +8,123 @@ public class Menu {
     static void start() throws ExitMenuCommand {
         SaveData.makeMembersFromData();
         System.out.println(welcome);
-        boolean menu = true;
-        while (menu) {
-            menu = Menu.mainMenu();
+
+        while (true) {
+            try
+            {
+                Menu.mainMenu();
+            }
+            catch (ExitMenuCommand _) {return;}
         }
     }
 
-    static boolean mainMenu() throws ExitMenuCommand {
-        boolean subMenu = true;
-            String options = "Tryk 1:\t\t\tTryk 2:\t\t\tTryk 3:" +
-                    "\nForperson\t\tKasserer\t\tTræner";
-            System.out.println("\n" + options);
+    static void mainMenu() throws ExitMenuCommand {
+        String options = "Tryk 1:\t\t\tTryk 2:\t\t\tTryk 3:" +
+                "\nForperson\t\tKasserer\t\tTræner";
 
-            Scanner sc = new Scanner(System.in); // TODO: benyt inquire
-            String optionsAnswer = sc.next();
+        System.out.println("\n" + options);
+        String optionsAnswer = UI.inquire(); // throws to exit program.
 
-            switch (optionsAnswer) { // TODO: put switch i try/catch ExitMenuCommand
-                case "1":
-                    while (subMenu) {
-                        subMenu = Menu.chairmanMenu();
-                    }
-                    break;
-                case "2":
-                    while (subMenu) {
-                        subMenu = Menu.cashierMenu();
-                    }
-                    break;
-                case "3":
-                    while (subMenu) {
-                        subMenu = Menu.coachMenu();
-                    }
-                    break;
-                case "q":
-                    return false;
+        try
+        {
+            while (true)
+            {
+                switch (optionsAnswer)
+                {
+                    case "1":
+                        Menu.chairmanMenu();
+                        break;
+                    case "2":
+                        Menu.cashierMenu();
+                        break;
+                    case "3":
+                        Menu.coachMenu();
+                        break;
+                }
             }
-            return true;
         }
+        catch (ExitMenuCommand _) {}
+    }
 
-    static boolean chairmanMenu() throws ExitMenuCommand {
+    static void chairmanMenu() throws ExitMenuCommand {
         String chairmanOptions =
                 "Tryk 1: Opret ny medlem" +
                         "\nTryk 2: Rediger eksisterende medlem" +
                         "\nTryk 3: Slet medlem" +
                         "\n\nTryk q: Hovedmenu";
+
         System.out.println(chairmanOptions);
-        Member referenceMember;
+        String chairmanAnswer = UI.inquire();
 
-        Scanner sc = new Scanner(System.in); // TODO: benyt inquire
-        String chairmanAnswer = sc.next();
-
-        switch (chairmanAnswer){ // TODO: put switch i try/catch ExitMenuCommand
-            case "1":
-                MemberRegister.addMemberMenu();
-                break;
-            case "2":
-                EditMember.edit();
-                break;
-            case "3":
-                DeleteMember.deleteMemberMenu();
-                break;
-            case "q":
-                return false;
-        }   return true;
+        try
+        {
+            switch (chairmanAnswer)
+            {
+                case "1":
+                    MemberRegister.addMemberMenu();
+                    break;
+                case "2":
+                    EditMember.edit();
+                    break;
+                case "3":
+                    DeleteMember.deleteMemberMenu();
+                    break;
+            }
+        }
+        catch (ExitMenuCommand _) {}
     }
 
-    static boolean cashierMenu(){
+    static void cashierMenu() throws ExitMenuCommand {
         String cashierOptions =
                 "Tryk 1: Se medlemmer i restance" +
                         "\nTryk 2: Registrer modtaget betaling" +
                         "\n\nTryk q: Hovedmenu";
+
         System.out.println(cashierOptions);
+        String cashierAnswer = UI.inquire();
 
-        Scanner sc = new Scanner(System.in); // TODO: benyt inquire
-        String cashierAnswer = sc.next();
-
-        switch (cashierAnswer){ // TODO: put switch i try/catch ExitMenuCommand
-            case "1":
-                PayMenu.paymentMenu();
-
-                break;
-            case "2":
-                PayMenu.makePayment();
-
-                break;
-            case "q":
-                return false;
-        } return true;
+        // try
+        // {
+            switch (cashierAnswer)
+            {
+                case "1":
+                    PayMenu.paymentMenu();
+                    break;
+                case "2":
+                    PayMenu.makePayment();
+                    break;
+            }
+        // }
+        // catch (ExitMenuCommand _) {}
     }
 
-    static boolean coachMenu() throws ExitMenuCommand {
+    static void coachMenu() throws ExitMenuCommand {
         String coachOptions =
                 "Tryk 1: Se hold" +
                         "\nTryk 2: Se disciplin" +
                         "\nTryk 3: Se medlem" +
                         "\n\nTryk q: Hovedmenu";
+
         System.out.println(coachOptions);
+        String coachAnswer = UI.inquire();
 
-        Scanner sc = new Scanner(System.in); // TODO: benyt inquire
-        String coachAnswer = sc.next();
-
-        switch (coachAnswer){ // TODO: put switch i try/catch ExitMenuCommand
-            case "1": // se hvilke svømmere i disciplin
-                System.out.println("HER SKAL TILFØJES NOGET: class Manu ->" +
-                        " forpersonMenu -> switch(coachAnswer)-> case 1");
-                break;
-            case "2":
-                coachViewDiscipline();
-                break;
-            case "3": // se medlem og deres præstationer
-                coachViewMember();
-                break;
-            case "q":
-                return false;
-        } return true;
+        try
+        {
+            switch (coachAnswer)
+            {
+                case "1": // se hvilke svømmere i disciplin
+                    System.out.println("HER SKAL TILFØJES NOGET: class Manu ->" +
+                            " forpersonMenu -> switch(coachAnswer)-> case 1");
+                    break;
+                case "2":
+                    coachViewDiscipline();
+                    break;
+                case "3": // se medlem og deres præstationer
+                    coachViewMember();
+                    break;
+            }
+        }
+        catch (ExitMenuCommand _) {}
     }
 
     static void coachViewMember() throws ExitMenuCommand
@@ -147,6 +151,7 @@ public class Menu {
             }
         }
     }
+
     static void coachViewDiscipline() throws ExitMenuCommand {
 
         Discipline discipline;
