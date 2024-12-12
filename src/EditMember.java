c28ee8e9203042c3bd0f4145e015a00332f8e23e
Simon Gotfred <simongotfred@hotmail.com>
2024-12-12 13:00:51 +0100

    Refactor i 'EditMember'
    - to english naming of methods

--- src/EditMember.java
+++ src/EditMember.java
@@ -16,18 +16,18 @@ public class EditMember {
            try {
 
                  switch (input.toLowerCase()){                                                                // Brug en switch-case til at håndtere brugerens valgcase "1" "telefon nummer", "tlf" : editTelefonnummer(member); break;                         // Kald metoden til at redigere telefonnummeret
-                 case "1""aktiv", "a": member.setActive(); break;
-                 case "2 ", "passiv", "p": member.setPassive(); break;
+                 case "1", "aktiv", "a": member.setActive(); break;
+                 case "2", "passiv", "p": member.setPassive(); break;
                  case "3", "slet", "delete", "s" : MemberRegister.getMembers().remove(member); break;
                  case "4", "konkurrence", "k" : member.setCompetitor(); break;
-                 case "5", "navn" , "n" : editname(member); break;
+                 case "5", "navn" , "n" : editName(member); break;
                  default:
                    System.out.println("Ugyldigt valg, prøv igen.");                                     // Håndterer ugyldige indtastninger
                 }
            } catch (AbortToMenuCommand e) {}
        }
     }
-    public static void editTelefonnummer(Member member) throws AbortToMenuCommand
+    public static void editPhoneNumber(Member member) throws AbortToMenuCommand
     {                                            // Metode til at redigere et medlems telefonnummer
         String input = UI.inquire("Hvad er det nye telefon nummer du gerne vil redigere?");         // Spørg brugeren om det nye telefonnummer
         if (Member.isPhoneNumber(input)){                                                           // Tjek, om telefonnummeret er korrekt formateret
@@ -35,10 +35,10 @@ public class EditMember {
         }
         else {
             System.out.println("Forkert formateret, prøv igen");                                    // Hvis formateringen er forkert, informer brugeren og prøv igen
-            editTelefonnummer(member);
+            editPhoneNumber(member);
         }
     }
-    public static void editname(Member member) throws AbortToMenuCommand
+    public static void editName(Member member) throws AbortToMenuCommand
     {
         String input = UI.inquire("Hvad er navnet du vil ændre til?");
         if (Member.isName(input)){
@@ -46,7 +46,7 @@ public class EditMember {
         }
         else{
             System.out.println("Forkert formateret, prøv igen");
-            editname(member);
+            editName(member);
         }
     }
 }
